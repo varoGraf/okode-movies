e33b45ce666abab01ea8865a18ba994a92d8e549
varoGraf <avox97@gmail.com>
2021-04-04 17:25:53 +0200

    HTML movie list bar working

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieService, SearchType } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  constructor(private movieService : MovieService) { }
 
   ngOnInit() {
+  }
+
+  searchChanged()
+  {
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+    
   }
 
 }
