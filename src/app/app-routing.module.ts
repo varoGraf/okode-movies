c1ed3ad2cee72f82e3d38b9a96a3a2f001b59994
varoGraf <avox97@gmail.com>
2021-04-04 16:07:04 +0200

    Home_Erased+Pages_Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /*{
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
@@ -14,6 +14,17 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },*/
+  {path: '', redirectTo: 'movies', pathMatch:'full'},
+  {path:'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule'},
+  {path: 'movie-details', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule'},
+  {
+    path: 'movies',
+    loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'movie-details',
+    loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   },
 ];
 
