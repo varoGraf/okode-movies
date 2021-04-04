1e5c7e3bd4ae320b9ee1b2a9069d6e68da0f95a4
varoGraf <avox97@gmail.com>
2021-04-04 16:42:11 +0200

    Service request API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,28 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  /*{
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'message/:id',
-    loadChildren: () => import('./view-message/view-message.module').then( m => m.ViewMessagePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },*/
   {path: '', redirectTo: 'movies', pathMatch:'full'},
-  {path:'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule'},
-  {path: 'movie-details', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule'},
   {
     path: 'movies',
     loadChildren: () => import('./pages/movies/movies.module').then( m => m.MoviesPageModule)
   },
   {
-    path: 'movie-details',
+    path: 'movies/:id',
     loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   },
 ];
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+
+export enum SearchType 
+{
+  all='',
+  movie='movie',
+  series = 'series',
+  episode = 'episode'
+}
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  url = 'https://www.omdbapi.com/';
+  apiKey='cfc5ce3b';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  searchData(title: string, type: SearchType): Observable<any>{ 
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => {
+        console.log('RAW: ', results);
+        return results['Search'];
+      })
+    );
+  }
+
+  getDetails(id){
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
